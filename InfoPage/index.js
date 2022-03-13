29d813583bb012b1a14aae65c8af200ca9ca0fb6
abondar24 <abondar1992@gmail.com>
2022-03-13 12:16:15 +0100

    components added

--- InfoPage/index.js
+++ InfoPage/index.js
@@ -1,14 +1,48 @@
-const list =  (
-  <div>
-    <img src="./react-logo.png" width="40px"/>
-    <h1>Welcome to React world</h1>
+
+function Header(){
+  return (
+    <header>
+      <nav className="nav">
+        <img src="./react-logo.png" className="nav-logo"/>
+        <ul className="nav-items">
+          <li>React</li>
+          <li>Node</li>
+          <li>Babel</li>
+        </ul>
+      </nav>
+    </header>
+  )
+}
+
+function Footer (){
+  return (
+    <footer>
+      <small>2022 Experimental development </small>
+    </footer>
+
+  )
+}
+
+function List(){
+  return (
     <ul>
       <li>Components</li>
       <li>JSX</li>
       <li>redux</li>
     </ul>
+  )
+}
 
-  </div>
-)
 
-ReactDOM.render(list,document.getElementById("root"))
+function Page(){
+  return (
+    <div>
+    <Header />
+    <h1>Welcome to React world</h1>
+    <List/>
+    <Footer />
+    </div>
+  )
+}
+
+ReactDOM.render(<Page />,document.getElementById("root"))
