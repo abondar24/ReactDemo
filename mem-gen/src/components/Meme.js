b2fc5f8eb822ad41826981cb4dc53a4470a8f419
abondar24 <abondar1992@gmail.com>
2022-03-29 22:03:00 +0200

    mem-gen: header form

---
+++ mem-gen/src/components/Meme.js
@@ -0,0 +1,23 @@
+import '../stylesheets/Meme.css';
+
+
+export default function Meme(){
+
+  return (
+      <main>
+         <form className="form">
+           <input type="text"
+                  placeholder="Top text"
+                  className="form-input"
+          />
+           <input type="text"
+                  placeholder="Bottom text"
+                  className="form-input"
+          />
+           <button className="form-button">
+                  Get a new image &#128512;
+          </button>
+         </form>
+      </main>
+  )
+}
