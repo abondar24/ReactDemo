338300b956c0c908a7576bbbab919c0d375a6e46
abondar24 <abondar1992@gmail.com>
2022-04-08 22:42:10 +0200

    notes: editor base from scrimba

---
+++ notes/src/components/Sidebar.js
@@ -0,0 +1,29 @@
+import '../stylesheets/Sidebar.css';
+
+
+function Sidebar(props) {
+    const noteElems = props.notes.map((note, index) => (
+        <div key={note.id}>
+            <div className={`title ${note.id == props.currentNote.id ? "selected-note" : ""
+                }`}
+                onClick={() => props.setCurrentNodeId(note.id)}
+            >
+                <h4 className="text-snippet">Note {index + 1}</h4>
+            </div>
+        </div>
+    ))
+
+    return (
+        <section className="pane sidebar">
+            <div className="sidebar-header">
+                <h3>Notes</h3>
+                <button className="new-note" onClick={props.newNote}>+</button>
+            </div>
+            {noteElems}
+        </section>
+    )
+
+}
+
+
+export default Sidebar
