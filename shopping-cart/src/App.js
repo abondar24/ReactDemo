8b534f138666fd122045f30bbf19591679807c21
abondar24 <abondar1992@gmail.com>
2022-04-18 19:23:14 +0200

    cart: fetch send data to firebase

--- shopping-cart/src/App.js
+++ shopping-cart/src/App.js
@@ -2,18 +2,41 @@ import { useSelector, useDispatch } from "react-redux"
 import { actions } from "./store/index"
 import Login from "./components/Login"
 import Layout from "./components/Layout";
+import { useEffect } from "react";
 
 function App() {
 
   const isLoggedIn = useSelector(state => state.login.isLoggedIn);
+  const cart = useSelector(state => state.cart);
+
+
+  useEffect(() => {
+
+    const sendRequest = async () => {
+      const resp = await fetch(
+        "https://shoppingcart-a62bb-default-rtdb.europe-west1.firebasedatabase.app/cartItems.json",
+        {
+          method: "PUT",
+          body: JSON.stringify(cart)
+        });
+
+      const data = await resp.JSON();
+
+
+    }
+
+    sendRequest();
+  }, [cart]);
 
   return (
     <div>
       {!isLoggedIn && <Login />}
-      {isLoggedIn && <Layout/>}
-     
+      {isLoggedIn && <Layout />}
+  
     </div>
   );
+  
 }
+
 
 export default App;
