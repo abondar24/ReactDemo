9afe15c9f253dcb0b1e9f1b362c8942c94ca82a8
abondar24 <abondar1992@gmail.com>
2022-04-18 15:23:24 +0200

    cart: login clean up

--- shopping-cart/src/components/Login.js
+++ shopping-cart/src/components/Login.js
@@ -1,19 +1,19 @@
-import React  from 'react';
+import React  from "react";
 
-import '../stylesheets/Login.css' 
+import "../stylesheets/Login.css" 
 
 
 function Login(){
   return(
-        <div className='login-container'>
-        <h1>Login</h1>{" "}
+        <div className="login-container">
+        <h1 className="login-h1">Login</h1>
         <form>
-            <label htmlFor='id'>Account id</label>
-            <input type='text' name='id' id='id'/>
+            <label htmlFor="id">Account id</label>
+            <input type="text" name="id" id="id"/>
  
-            <label htmlFor='password'>Password</label>
-            <input type='password' name='password' id='password'/>
-            <button className='login-btn' type='submit'>
+            <label htmlFor="password">Password</label>
+            <input type="password" name="password" id="password"/>
+            <button className="login-btn" type="submit">
                 Login
             </button>
         </form>
